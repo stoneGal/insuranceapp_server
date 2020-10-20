cca750fbcf838dbccfbca43626c05931c31ad2db
Gharoro <gharoropureheart@gmail.com>
2020-10-20 20:00:04 +0100

    did nothing

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const morgan = require("morgan");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
-const connect = require('./config/dbconnection');
+const connect = require("./config/dbconnection");
 
 connect();
 
 //middleware
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(express.json()); //req.body
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -18,17 +18,23 @@ app.use(express.urlencoded({ extended: true }));
 //Routers
 
 //register and login routes
-app.use('/auth', require('./routes/jwtAuth'));
+app.use("/auth", require("./routes/jwtAuth"));
 
 //dashboard router
-app.use('/dashboard', require('./routes/dashboard'));
+app.use("/dashboard", require("./routes/dashboard"));
 
 //policy router
-app.use('/policy', require('./routes/policy'));
+app.use("/policy", require("./routes/policy"));
 
 //beneficiary router
-app.use('/beneficiary', require('./routes/beneficiary'));
-app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/beneficiary", require("./routes/beneficiary"));
+
+// home route
+app.get("/home", (req, res) => {
+  res.send("Bethel Insurance Application API");
+});
+
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // app.use((req, res, next)=> {
 //     const err = new Error();
@@ -46,6 +52,8 @@ app.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 //     });
 // }
 
-app.listen(5000, () => {
-  console.log('App has started at port 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("App has started at port 5000");
 });
